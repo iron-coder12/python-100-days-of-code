5155004c505e940aed322d433a4dabc2856bb4d0
Iron Coder <79589166+iron-coder12@users.noreply.github.com>
2021-05-21 14:36:48 +0530

    add files via upload
    this is the day 2 folder

---
+++ Day 2/datatypes.py
@@ -0,0 +1,22 @@
+#DATA TYPES
+#Strings
+
+print("Hello")
+print("Hello"[0])
+print("Hello"[4])
+
+"123"
+
+#Integer
+
+print(123 + 345)
+print(123_456_789)
+
+#Boolean
+
+True
+False
+
+#Float
+
+3.14159
