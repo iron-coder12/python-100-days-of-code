fb318bbd9e932f4ff0cd45a3f4e06c0d84d2b0a2
Shaurya Rathi <79589166+iron-coder12@users.noreply.github.com>
2021-09-10 20:57:36 +0530

    Add files via upload

---
+++ Day 8/start1.py
@@ -0,0 +1,16 @@
+#def greet():
+ #   print("Hi!")
+  #  print("How do you do?")
+   # print("Isn't the weather nice today?")
+#greet()
+
+#def greet_with_name(name):
+ #   print(f"Hello {name}")
+  #  print(f"How do you do {name}?")
+#greet_with_name("Name")
+
+def greet_with(name, location):
+   print(f"Hello {name}")
+   print(f"What is it like in your {location}?")
+greet_with(location="London", name="Shaurya")
+
