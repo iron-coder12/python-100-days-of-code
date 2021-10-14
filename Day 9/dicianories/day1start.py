bc680ea5c58b6f054ff5ae6355641b1970033137
Shaurya Rathi <79589166+iron-coder12@users.noreply.github.com>
2021-10-14 15:38:57 +0530

    Add files via upload

---
+++ Day 9/dicianories/day1start.py
@@ -0,0 +1,11 @@
+programming_dictionary = {"Bug": "An err in a program that prevents it from running as expected.",
+"Function": "A piece of code that you can easily call over and over again."}
+
+print(programming_dictionary["Bug"])
+
+# Adding new items to dictionary.
+programming_dictionary["loop"] = "The action og doing something over and over again"
+
+print(programming_dictionary)
+
+empty_list = []
---
+++ Day 9/dicianories/nesting.py
@@ -0,0 +1,12 @@
+travel_log = {
+    "France": {'cities_visited':["Paris", "Disney Land", "Dijon"], "toal_visits": 12},
+    "Germany": {'cities_visited':["Berlin", "Hamburg", "Stuggart",], "toal_visits": 5}
+}
+
+
+#Nesting Dictianories In a list
+
+travel_log = {
+    {"Country": 'France', 'cities_visited':["Paris", "Disney Land", "Dijon"], "toal_visits": 12},
+    {"Country": 'Germany', 'cities_visited':["Berlin", "Hamburg", "Stuggart",], "toal_visits": 5}
+}
