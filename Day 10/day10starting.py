7df5a9c11b528cb4fea4fb1fc2c690d686c6f1e1
Shaurya Rathi <79589166+iron-coder12@users.noreply.github.com>
2021-10-30 08:00:12 +0530

    Add files via upload

---
+++ Day 10/day10starting.py
@@ -0,0 +1,12 @@
+# Functions With Outputs
+
+def format_name(f_name, l_name):
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"{formated_f_name} {formated_l_name}"
+
+
+print(
+    format_name("angela", "YU")
+)
+# Tony Stark
