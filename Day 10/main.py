ddd08e6e7562736694ffc4fa0de94f4d7d6e4916
Shaurya Rathi <79589166+iron-coder12@users.noreply.github.com>
2021-11-04 15:24:38 +0530

    Add files via upload

---
+++ Day 10/main.py
@@ -0,0 +1,47 @@
+
+
+# Addition
+
+def add(n1, n2):
+    return n1 + n2
+
+# Subtraction
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+# Multiplication
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+# Division
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+num1 = int(input("What's the first number?: "))
+
+for symbol in operations:
+    print(symbol)
+
+operation_symbol = input("Pick an operation from the line above: ")
+
+num2 = int(input("What's the second number?: "))
+
+calculation_function = operations[operation_symbol]
+
+answer = calculation_function(num1, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
